9fe39699a2210fb548a4feeb753d873b8be0d876
md rafiul islam <rafiulislam2001@gmail.com>
2025-09-15 22:16:39 +0600

    active btn functionality added

--- script/index.js
+++ script/index.js
@@ -19,7 +19,7 @@ const displayLessonBtn = (jsonData) => {
     const btnDiv = document.createElement("div");
     // console.log(info);
     btnDiv.innerHTML = `
-        <button onclick="loadLessonWords(${info.level_no})" class="btn btn-outline btn-primary">
+        <button onclick="loadLessonWords(${info.level_no})" id="btn-${info.level_no}" class="btn btn-outline btn-primary lesson-btn">
             <i class="fa-solid fa-book-open"></i>Learn-${info.level_no}
         </button>
     `;
@@ -28,9 +28,20 @@ const displayLessonBtn = (jsonData) => {
   }
 };
 
+//active btn remove
+const removeActive = () => {
+  const lessonBtn = document.querySelectorAll(".lesson-btn");
+  console.log(lessonBtn);
+  for (const xyz of lessonBtn) {
+    xyz.classList.remove("active");
+  }
+};
 // lessons word
 const loadLessonWords = (level) => {
-  //   console.log(level);
+  //active btn add
+  const lsnBtn = document.getElementById(`btn-${level}`);
+  removeActive();
+  lsnBtn.classList.add("active");
 
   const url = `https://openapi.programming-hero.com/api/level/${level}`;
   fetch(url)
